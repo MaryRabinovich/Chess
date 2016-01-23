8967287d53c88c05026637205eb433485cf95e2f
MaryRabinovich <mary.rabinovich@mail.ru>
2016-01-23 19:01:23 +0300

    Метод может сам себя вызывать, как факториал. Как это использовать?

--- ChessCalculationProcedural/ChessCalculationProcedural/Program.cs
+++ ChessCalculationProcedural/ChessCalculationProcedural/Program.cs
@@ -10,6 +10,37 @@ namespace ChessCalculationProcedural
     {
         static void Main(string[] args)
         {
+            int DeskSize = 3;
+            int[] Line = new int[DeskSize];
+
+            for (int i = 0; i < DeskSize; i++)
+            {
+                Line[i] = i;
+                Console.WriteLine(Line[i]);
+            }
+
+            Console.WriteLine();
+
+            int LocalDeskSize = 1;
+            int[] NewLine = Permutation(Line, LocalDeskSize);
+            for (int i = 0; i < DeskSize; i++)
+            {
+                Console.WriteLine(NewLine[i]);
+            }
+
+            Console.ReadLine();
         }
+
+
+        // метод может сам себя вызывать: http://professorweb.ru/my/csharp/charp_theory/level5/5_11.php
+
+        static int[] Permutation(int[] Line, int LocalDeskSize)
+        {
+            if (LocalDeskSize == 1)
+            {
+                return Line;
+            }
+            else return Line;
+        } 
     }
 }
