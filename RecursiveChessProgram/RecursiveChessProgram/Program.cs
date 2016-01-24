aa63f5840325be002590fd67b12a65fc981a144d
MaryRabinovich <mary.rabinovich@mail.ru>
2016-01-24 17:51:30 +0300

    Yeah! I've done it!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- RecursiveChessProgram/RecursiveChessProgram/Program.cs
+++ RecursiveChessProgram/RecursiveChessProgram/Program.cs
@@ -16,21 +16,24 @@ namespace RecursiveChessProgram
             int[,] Desk = new int[DeskSize, DeskSize];
             Desk = CreateEmptyDesk(DeskSize);
 
+            Console.WriteLine("Вот они, правильные доски: ");
+            Console.WriteLine();
+
             int[] NumberOfFittingDesks = new int[1];
             NumberOfFittingDesks[0] = PutQueensOnTheDesk(Desk, DeskSize, 0, NumberOfFittingDesks);
-            Console.Write("Количество нужных досок равно ");
+
+            Console.Write("Количество правильных досок равно ");
             Console.Write(NumberOfFittingDesks[0]);
             Console.ReadLine();
-
 
         }
 
         static int GetDeskSize()
         {
-            int DeskSize = 3;
+            int DeskSize = 4;
             Console.WriteLine();
             Console.WriteLine("Нажмите любую клавишу");
-            Console.Write("(это я, функция GetDeskSize, сейчас я задаю доску ширины ");
+            Console.Write("(я - функция GetDeskSize, сейчас я задаю доску ширины ");
             Console.Write(DeskSize);
             Console.WriteLine(" )");
             Console.WriteLine();
@@ -53,7 +56,6 @@ namespace RecursiveChessProgram
             for (int Col = 0; Col < DescSize; Col++)
                 for (int Row = 0; Row < DescSize; Row++)
                     NewDesk[Col, Row] = Desk[Col, Row];
-           // ShowDesk(NewDesk, DescSize);
             return NewDesk;
         }
 
@@ -66,11 +68,6 @@ namespace RecursiveChessProgram
                 Console.WriteLine();
             }
             Console.WriteLine();
-            /*
-            Console.WriteLine("Нажмите любую клавишу");
-            Console.WriteLine("(это я, функция ShowDesk)");
-            Console.ReadLine();
-            */
         }
 
         static int PutQueensOnTheDesk(int[,] Slice, int DeskSize, int SliceIndex, int[] NumberOfFittingDesks)
@@ -82,26 +79,87 @@ namespace RecursiveChessProgram
                     int[,] NewSlice = new int[DeskSize, DeskSize];
                     NewSlice = CopyDesk(Slice, DeskSize);
                     NewSlice[SliceIndex, i] = 1;
-                    if (SliceIndex == DeskSize - 1) ShowDesk(NewSlice, DeskSize);
                     PutQueensOnTheDesk(NewSlice, DeskSize, SliceIndex + 1, NumberOfFittingDesks);
                 }
             }
-            else NumberOfFittingDesks[0] = NumberOfFittingDesks[0] + CheckDesk(Slice, DeskSize, SliceIndex);
+            else NumberOfFittingDesks[0] = NumberOfFittingDesks[0] + CheckDesk(Slice, DeskSize);
 
             return NumberOfFittingDesks[0];
         }
 
-
-
-        // ЭТУ Я ДАЖЕ НЕ НАЧИНАЛА ТОЛКОМ.
-        // ТУТ ДОЛЖНА БЫТЬ ПРОВЕРКА КОНКРЕТИЗИРОВАННОЙ ДОСКИ НА ПАРШИВОСТЬ, ПРИ ЗАДАННОЙ РАССТАНОВКЕ ФЕРЗЕЙ
-        // СЕЙЧАС ОНА ПРОВЕРЯЕТ ВСЕГО ЛИШЬ УГЛОВОЙ ТЕРМИН:
-        // ЕСЛИ ЕСТЬ КОРОЛЕВА В ЛЕВОМ ВЕРХНЕМ УГЛУ, ЭТО ТИПА ПОКА ПОДХОДЯЩАЯ ДОСОЧКА
-        static int CheckDesk(int[,] Desk, int DeskSize, int SliceIndex)
+        static int CheckDesk(int[,] Desk, int DeskSize)
         {
-            return 1;
+            int DeskQuality = 1;
+            DeskQuality = DeskQuality * CheckColumns(Desk, DeskSize);
+            DeskQuality = DeskQuality * CheckRows(Desk, DeskSize);
+            DeskQuality = DeskQuality * CheckXplusY(Desk, DeskSize);
+            DeskQuality = DeskQuality * CheckYminusX(Desk, DeskSize);
+
+            if (DeskQuality == 1) ShowDesk(Desk, DeskSize);
+
+            return DeskQuality;
         }
 
+        static int CheckColumns(int[,] Desk, int DeskSize)
+        {
+            int FitDesk = 1;
+            int ColSum; 
+            for (int Col = 0; Col < DeskSize; Col++)
+            {
+                ColSum = 0;
+                for (int Row = 0; Row < DeskSize; Row++) ColSum = ColSum + Desk[Col, Row];
+                if (ColSum != 1) FitDesk = 0;
+            }
+            return FitDesk;
+        } 
 
+        static int CheckRows(int[,] Desk, int DeskSize)
+        {
+            int FitDesk = 1;
+            int RowSum;
+            for (int Row = 0; Row < DeskSize; Row++)
+            {
+                RowSum = 0;
+                for (int Col = 0; Col < DeskSize; Col++) RowSum = RowSum + Desk[Col, Row];
+                if (RowSum != 1) FitDesk = 0;
+            }
+            return FitDesk;
+        } 
+
+        static int CheckXplusY(int[,] Desk, int DeskSize)
+        {
+            int FitDesk = 1;
+            int DiagonalSum;
+            int Row = 0;
+            for (int XplusY = 0; XplusY < 2*DeskSize; XplusY++)
+            {
+                DiagonalSum = 0;
+                for (int Col = 0; Col < DeskSize; Col++)
+                {
+                    Row = XplusY - Col;
+                    if (Row > -1 && Row < DeskSize) DiagonalSum = DiagonalSum + Desk[Col, Row];
+                }
+                if (DiagonalSum > 1) FitDesk = 0;
+            }
+            return FitDesk;
+        } 
+
+        static int CheckYminusX(int[,] Desk, int DeskSize)
+        {
+            int FitDesk = 1;
+            int DiagonalSum;
+            int Row = 0;
+            for (int YminusX = - DeskSize; YminusX < DeskSize; YminusX++)
+            {
+                DiagonalSum = 0;
+                for (int Col = 0; Col < DeskSize; Col++)
+                {
+                    Row = Col - YminusX;
+                    if (Row > -1 && Row < DeskSize) DiagonalSum = DiagonalSum + Desk[Col, Row];
+                }
+                if (DiagonalSum > 1) FitDesk = 0;
+            }
+            return FitDesk;
+        }
     }
 }
